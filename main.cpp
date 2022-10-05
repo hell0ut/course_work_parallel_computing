a43b41c73d0f8fb934304a82202fa7f8eac7a3c9
hell0ut <43760986+hell0ut@users.noreply.github.com>
2022-10-05 12:45:59 +0300

    feat:add ThreadStorage class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <thread>
+#include <filesystem>
+
+const int NUMBER_OF_THREADS = 8;
+
+
+class ThreadStorage{
+public:
+    std::vector<std::thread> threads;
+    explicit ThreadStorage(int number_of_threads){
+    threads = std::vector<std::thread>(number_of_threads);
+}
+
+};
 
 int main(){
-    std::cout<<"Hello";
-    return 0;
+    ThreadStorage threadStorage(NUMBER_OF_THREADS);
+
+
+
 }
